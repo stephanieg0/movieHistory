3589d488915e140853cebf38460920cfc4b778a9
stephanie miranda <stephanie.miranda.valdez@gmail.com>
2015-11-19 13:46:59 -0600

    added basic code

---
+++ javascripts/dependencies.js
@@ -0,0 +1,8 @@
+define(function(require){
+	var $ = require("jquery");
+	var hbs = require("hbs");
+	var bootstrap = require("bootstrap");
+	var lodash = require("lodash");
+	var Firebase = require("firebase");
+	var Q = require("q");
+});
---
+++ javascripts/user-login.js
@@ -0,0 +1,33 @@
+define(function(require){
+	var $ = require("jquery");
+	var Firebase = require("firebase");
+
+
+
+	var ref = new Firebase("https://movie-history-app.firebaseio.com");
+
+	//creating user
+	ref.createUser({
+  		email    : $("#email").val(),
+  		password : $("#password").val()
+	}, function(error, userData) {
+	  if (error) {
+	    console.log("Error creating user:", error);
+	  } else {
+	    console.log("Successfully created user account with uid:", userData.uid);
+	  }
+	});
+
+	//login with password
+	ref.authWithPassword({
+	  email    : $("#email").val(),
+	  password : $("#password").val()
+	}, function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+	  }
+	});
+
+});//end of define
