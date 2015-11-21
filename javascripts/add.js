c8bc9ea166ace57c3626f0b122b301f5b56df72b
micahp0506 <micahp0506@gmail.com>
2015-11-21 09:09:38 -0600

    Updating add feature

--- javascripts/add.js
+++ javascripts/add.js
@@ -15,11 +15,29 @@ define(function(require){
       		// Getting movie object
       		var movieObject = gs.getData();
       		console.log("movieObject", movieObject);
+      		// Getting movie title from movie object
+      		var title = movieObject.Title;
+      		console.log("title", movieObject.Title);
+      		// Getting movie year from movie object
+      		var year = movieObject.Year;
+      		console.log("year", movieObject.Year);
+      		// Getting movie year from movie object
+      		var actors = movieObject.Actors;
+      		console.log("actors", movieObject.Actors);
+
+      		var poster = movieObject.poster
 			//get a reference to our Firebase app
 			var ref = new Firebase("https://movie-history-app.firebaseio.com/users/"+ uuid);
       		console.log("uuid", uuid);
 			ref.push({
-		  		"movie": "movie",
+				
+		  		"title": title,
+		  		"year": year,
+		  		"actors": actors,
+		  		"stars": 0,
+		  		"watched": false,
+		  		"poster": "http://img.omdbapi.com/?i=" + movieObject.imdbID + "&apikey=8513e0a1"
+
 			}, function(error, userData) {
 			  if (error) {
 			  	alert(error);
