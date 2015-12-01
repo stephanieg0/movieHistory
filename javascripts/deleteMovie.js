d1b8ead7cb938690b2acedc246ebe5b5dbcaefd1
stephanie miranda <stephanie.miranda.valdez@gmail.com>
2015-12-01 11:35:16 -0600

    added inactive key on firebase to filter

--- javascripts/deleteMovie.js
+++ javascripts/deleteMovie.js
@@ -7,8 +7,22 @@ define(function(require) {
 		// Getting unique user ID
 		var uuid = gs.getUid();
 		// Ref to Firebase
-		var ref = new Firebase("https://movie-history-app.firebaseio.com/users/" + uuid + "/movies");
-		// Deleting selected movie
-		ref.child(deleteThisMovie).remove();	 
+		var ref = new Firebase("https://movie-history-app.firebaseio.com/users/" + uuid + "/movies/" + deleteThisMovie);
+		// Deleting selected movie --old
+		// ref.child(deleteThisMovie).remove();	--old
+
+		//seting an inactive key on firebase to be able to filter later in the dom.
+		//This movie should stay in firebase, but not visible in the dom.
+		console.log("https://movie-history-app.firebaseio.com/users/" + uuid + "/movies/" + deleteThisMovie);	
+		//updating the value in firebase
+		ref.update({
+			"inactive": true
+			}, function(error, userData){
+				if (error) {
+					alert(error);
+				} else {
+					console.log("on firebase");
+				}	
+			});
 	 };
  });
